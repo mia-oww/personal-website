a962ee75069ad13d1c3826f371a4f838bf273591
mia-oww <miafendi01@gmail.com>
2025-07-09 03:35:01 -0500

    added new project in progress

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = 'django-insecure-)i_x8p*#5n$b^-tl-qpcgd7m0_+!904!vykhii0*db!exhu178'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
